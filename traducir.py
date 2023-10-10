e1a93f56ea1e7b806ab08725c22d654247a60b27
MiguelMercadoS <54323800+MiguelMercadoS@users.noreply.github.com>
2023-10-09 22:30:17 -0500

    Update traducir.py

--- traducir.py
+++ traducir.py
@@ -2,8 +2,23 @@ import streamlit as st
 from googletrans import Translator
 from gtts import gTTS
 from PIL import Image
-import easyocr
 import os
+from google.colab import files
+uploaded = files.upload()
+from PIL import Image
+import pytesseract
+import cv2
+
+img_cv = cv2.imread(r'/content/Becaimg.jpg')
+
+# By default OpenCV stores images in BGR format and since pytesseract assumes RGB format,
+# we need to convert from BGR to RGB format/mode:
+img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
+print(pytesseract.image_to_string(img_rgb))
+# OR
+img_rgb = Image.frombytes('RGB', img_cv.shape[:2], img_cv, 'raw', 'BGR', 0, 0)
+text_in=pytesseract.image_to_string(img_rgb)
+print(pytesseract.image_to_string(img_rgb))
 
 st.title("Traductor")
 image = Image.open('traductor.jpg')
@@ -12,9 +27,9 @@ try:
     os.mkdir("temp")
 except:
     pass
-st.subheader("Leer y traducir desde texto o imagen")
+st.subheader("Leer y traducir desde texto.")
 
-st.write('Usa la aplicación para traducir texto o texto de imágenes en un idioma que no hables')
+st.write('Usa el aplicativo cuando quieras decir algo en un idioma que no hables')
 source_lang = "es"  # Lenguaje de origen (puedes cambiarlo según tus necesidades)
 translator = Translator()
 text = st.text_input("Escribe el texto que quieres traducir aquí:")
@@ -27,12 +42,10 @@ languages = {
     "Portugués": "pr",
     "Francés": "fr",
     "Ruso": "ru",
-    "Chino Mandarín": "zh-cn",
+    "Chino Mandarín": "zh-cn", 
 }
-
 # Widget para seleccionar el idioma de destino
-target_lang = st.selectbox("¿En cuál idioma quieres traducirlo?:", list(languages.keys()))
-
+target_lang = st.selectbox("¿En cual idioma quieres traducirlo?:", list(languages.keys()))
 def text_to_speech(text, tld):
     tts = gTTS(text, lang=tld, slow=False)
     try:
@@ -41,7 +54,6 @@ def text_to_speech(text, tld):
         my_file_name = "audio"
     tts.save(f"temp/{my_file_name}.mp3")
     return my_file_name, text
-
 if text and target_lang:
     target_lang_code = languages[target_lang]
     translated_text = translator.translate(text, src=source_lang, dest=target_lang_code).text
@@ -56,30 +68,6 @@ if text and target_lang:
     st.audio(audio_bytes, format="audio/mp3", start_time=0)
     st.markdown(f"## Texto en audio:")
     st.write(f" {output_text}")
-
-# Sección para cargar una imagen con texto
-st.subheader("Cargar una imagen con texto:")
-uploaded_image = st.file_uploader("Carga una imagen:", type=["jpg", "png", "jpeg"])
-
-if uploaded_image:
-    image = Image.open(uploaded_image)
-    st.image(image, caption="Imagen cargada", use_column_width=True)
-    
-    # Detectar texto en la imagen
-    reader = easyocr.Reader(["en"])
-    detected_text = reader.readtext(uploaded_image)
-
-    # Extraer y mostrar el texto detectado
-    if detected_text:
-        extracted_text = " ".join([text for (text, _, _) in detected_text])
-        st.markdown("## Texto detectado en la imagen:")
-        st.write(extracted_text)
-
-        # Traducir el texto detectado
-        translated_text = translator.translate(extracted_text, src="en", dest=target_lang_code).text
-        st.markdown("## Texto traducido:")
-        st.write(translated_text)
-
-    
-
+image2 = Image.open('traducir.jpg')
+st.image(image2, width=250)
 
